e9e5feaf56f87904da2c42b2d89904250db13910
kristya <shiriagina@yahoo.com>
2020-04-26 17:28:11 +0200

    added solution from book to excersise PrintAllPrimesLessN

--- src/main/java/Chapter3_exercises/printAllPrimesLessN/printAllPrimeLessN.js
+++ src/main/java/Chapter3_exercises/printAllPrimesLessN/printAllPrimeLessN.js
@@ -2,23 +2,14 @@
 
 function PrintAllPrimesLessThenN(n)
 {
-    // var smallArray = [];
-    // smallArray.push(2);
-    // smallArray.push(3);
- //  if(n<=2){return false;}
- //  if(n>=5){console.log(smallArray);}
-
     var array =[2,3];
-   for (var i=5; i<n; i=i+6){
+    for (var i=5; i<n; i=i+6){
        array.push(i);
        array.push(i+2);
 
+    }
 
-   }
- // return console.log(array);
-
-console.log(array.forEach(isPrime))
-
+    console.log(array.forEach(isPrime))
 
 }
 
@@ -32,11 +23,8 @@ function isPrime(k){
         if(k%i == 0)return false;
     }
 
-
     console.log(k);
     return true;
-
-
 }
 
 PrintAllPrimesLessThenN(70);

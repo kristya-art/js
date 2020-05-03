e280f7f02d2acd6f0e0749f1bbdff22267a4ebc0
kristya <shiriagina@yahoo.com>
2020-05-03 23:42:59 +0200

    put it in function

--- src/main/java/chapter5_arrays/AddUp2aNumber.js
+++ src/main/java/chapter5_arrays/AddUp2aNumber.js
@@ -1,24 +1,41 @@
 //function AddUp2aNumber(n,array) {
 
-     array =[1,2,3,4,5,6];
-    var k = array.length;
-
-    for(var i=0; i<k;i++){
-        for(var j=0; j<k;j++){
-
-            pos=array[i];
-            if (SumUp2Number(array[i],array[j],9)==true){
-                console.log(array[i],array[j]);
-            }
-            else{console.log("There are no numbers that make this number by sum")}
-        }
-   // }
-
-}
+//      array =[1,2,3,4,5,6];
+//     var k = array.length;
+//
+//     for(var i=0; i<k;i++){
+//         for(var j=0; j<k;j++){
+//
+//             pos=array[i];
+//             if (SumUp2Number(array[i],array[j],9)==true){
+//                 console.log(array[i],array[j]);
+//             }
+//             else{console.log("There are no numbers that make this number by sum")}
+//         }
+//    // }
+//
+// }
 
 function SumUp2Number(a,b,c){
     if((a+b)===c){return true;}
     return -1;
 }
+function AddUp2Number(n,array) {
+    var k = array.length;
 
+    for (var i = 0; i < k; i++) {
+        for (var j = 0; j < k; j++) {
+
+            pos = array[i];
+            if (SumUp2Number(array[i], array[j], n) == true) {
+                console.log(array[i], array[j]);
+             }
+            //else {
+            //     console.log("There are no numbers that make this number by sum")
+            // }
+        }
+    }
+}
 console.log(SumUp2Number(3,5,9) );
+
+AddUp2Number(9,[1,2,3,4,5,6]);

2593620c419832dc094622df5ec0556d03db2c42
kristya <shiriagina@yahoo.com>
2020-08-07 13:05:38 +0200

    added description to the program and adde else if statement to the end of this program in case if we have just 1 common element

--- src/main/java/chapter5_arrays/CommonElementsIn3Arrays.js
+++ src/main/java/chapter5_arrays/CommonElementsIn3Arrays.js
@@ -1,4 +1,9 @@
+/**
+ * This is a program that looking for common elements in 3 given arrays
+*/
+
 function CommonIn3(arr, arr1,arr2) {
+    //arr4 we will use to store the common numbers we will find
 var arr4=[];
     for(var i=0; i<arr.length;i++){
         for(var j=0;j<arr1.length; j++){
@@ -7,17 +12,19 @@ var arr4=[];
                     if(arr[i]===arr2[t]){
                       //  console.log([arr[i]]+' the common element in these 3 arrays');
                         arr4.push(arr[i]);
-                        arr1.splice(j,1);
+                        arr1.splice(j,1); // we delete the found element, cuz we don't need redundancy
                         arr2.splice(t,1);
-                        break;
+                        break; // we break here and start from next i element, cuz continue doesn't have sense
                     }
                 }
             }
         }
     }
-if(arr4.length>0) {
-    console.log(arr4 + ' Are common elements of these 3 arrays');
-}else (console.log('There are no common elements in these 3 arrays'));
+if(arr4.length>1) {
+
+    console.log(arr4 + ' Are common elements of these 3 arrays');}
+else if(arr4.length===1){console.log(arr4+ ' Is a common element of these 3 arrays')}
+else (console.log('There are no common elements in these 3 arrays'));
 }
 
- CommonIn3([1,2,5],[1,5,6,7,8,9,10,2],[4,8,9]);
+ CommonIn3([1,2,5],[1,5,6,7,8,9,10,2],[4,2,8,9]);
